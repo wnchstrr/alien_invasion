7cd29c0267de4919043c8f6b0d27e0ad74496da3
wnchstrr <haligali1592@gmail.com>
2026-04-18 21:28:34 +0500

    add alien invasion + gitignore

---
+++ alien_invasion.py
@@ -0,0 +1,96 @@
+import sys
+
+import pygame
+
+from bullet import Bullet
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__(self):
+        """ "Инициализирует игру и создает новые игровые ресурсы"""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        pygame.display.set_caption("Alien Invasion")
+        # Задание цвета фона.
+        self.bg_color = (0, 0, 255)
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+    def _check_events(self):
+        """Обрабатывает нажатия клавиш и события мыши"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Реагирует на нажатие клавиш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """Реагирует на отпускание клавиш"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Создает новый снаряд и добавляет его в группу bullets."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def run_game(self):
+        """Запускает основной цикл игры"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self.update_bullets()
+            # При каждом проходе цикла перерисовывается экран.
+            self._update_screen()
+            self.clock.tick(60)
+
+    def update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды"""
+        # Обновление позиций снарядов.
+        self.bullets.update()
+
+        # Удаление снарядов, вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """Обновляет изображения на экране и отображает новый экран"""
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    """Создание экземпляра и запуск игры"""
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    """Класс для хранения всех настроек игры "Инопланетное вторжение"."""
+
+    def __init__(self):
+        """Инициализирует настройки игры"""
+        # Параметры экрана.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (25, 0, 51)
+        self.bullets_allowed = 3    
+        # Настройки корабля
+        self.ship_speed = 1.5
+
+        # Параметры снаряда
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
